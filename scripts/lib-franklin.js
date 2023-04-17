f682c42809bb55776286339e129d92e614bf18d6
Yaroslav Abrasimov <y.abrasimov@atwix.com>
2023-04-17 18:27:08 +0200

    Revert changes

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -321,8 +321,7 @@ export function decorateSections(main) {
     /* process section metadata */
     const sectionMeta = section.querySelector('div.section-metadata');
     if (sectionMeta) {
-      // const meta = readBlockConfig(sectionMeta);
-      const meta = {};
+      const meta = readBlockConfig(sectionMeta);
       Object.keys(meta).forEach((key) => {
         if (key === 'style') {
           const styles = meta.style.split(',').map((style) => toClassName(style.trim()));
